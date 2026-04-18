cfbc0bec9224533bbb7a9f81660d60e3188277d4
srd15623693749 <15623693749@163.com>
2026-04-18 17:35:54 +0800

    style: 重置全局样式并调整应用布局

--- price-change-frontend/src/main.ts
+++ price-change-frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './style.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router'
